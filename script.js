c222712ad27cd7d9e81ec3db009a17b775709b53
Monika <codingbymon@gmail.com>
2024-10-26 11:57:29 +0200

    filter

--- script.js
+++ script.js
@@ -234,4 +234,8 @@ const numberedBooks = books.map((book, index) => {
 })
 numberedBooks
 
+const thickBooksWithMovie = books.filter(book => book.pages > 500).filter(book => book.hasMovieAdaptation)
+thickBooksWithMovie
 
+const adventureBooks = books.filter(book => book.genres.includes('adventure')).map(book => book.title)
+adventureBooks
