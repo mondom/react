d66d8e8a1dd2d817036a089f5bc78c836114e10a
Monika <codingbymon@gmail.com>
2024-10-30 11:37:05 +0100

    async/await

--- script.js
+++ script.js
@@ -285,6 +285,16 @@
 
 // bookAfterUpdate
 
-fetch("https://jsonplaceholder.typicode.com/todos").then(res => res.json()).then(data => console.log(data))
+// fetch("https://jsonplaceholder.typicode.com/todos").then(res => res.json()).then(data => console.log(data))
 
-console.log("Monika")
+// console.log("Monika")
+
+
+async function getTodos() {
+	const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+	const data = await res.json()
+	return data
+}
+
+const todos = getTodos()
+console.log(todos);
