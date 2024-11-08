9f70cb9deb9f014d6809cd5adf6e4104c536f745
Monika <codingbymon@gmail.com>
2024-11-08 10:11:11 +0100

    Complementing the content of the Component Menu

--- 03_pizza_menu/src/index.js
+++ 03_pizza_menu/src/index.js
@@ -49,11 +49,9 @@ const pizzaData = [
 function App() {
 	return (
 		<div>
-			<Header/>
-			<Pizza />
-			<Pizza />
-			<Pizza />
-			<Footer/>
+			<Header />
+			<Menu />
+			<Footer />
 		</div>
 	)
 }
@@ -62,14 +60,26 @@ function Header() {
 	return <h1>Fast React Pizza Co.</h1>
 }
 
-function Menu() {}
+function Menu() {
+	return (
+		<div>
+			<h2>Our menu</h2>
+			<Pizza />
+			<Pizza />
+			<Pizza />
+			<Pizza />
+		</div>
+	)
+}
 
 function Footer() {
 	// return React.createElement('footer', null, "We're currently open!")
-	return <footer>{new Date().toLocaleTimeString().slice(0, -2)}We're currently open!</footer>
+	return (
+		<footer>
+			{new Date().toLocaleTimeString().slice(0, -2)} We're currently open!
+		</footer>
+	)
 }
-
-
 
 function Pizza() {
 	return (
