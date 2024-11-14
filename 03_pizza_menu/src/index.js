cf33b022f82719c37bb20e0a452192754ad81bd8
Monika <codingbymon@gmail.com>
2024-11-14 12:26:17 +0100

    style inline

--- 03_pizza_menu/src/index.js
+++ 03_pizza_menu/src/index.js
@@ -57,7 +57,8 @@ function App() {
 }
 
 function Header() {
-	return <h1 style={{color: 'red'}}>Fast React Pizza Co.</h1>
+	const style = { color: "red", fontSize: "32px", textTransform: "uppercase" }
+	return <h1 style={style}>Fast React Pizza Co.</h1>
 }
 
 function Menu() {
