beb3b4bb0b0365149f80bd35f4db3697bc4bbc11
Monika <codingbymon@gmail.com>
2024-11-14 14:22:52 +0100

    className

--- 03_pizza_menu/src/index.js
+++ 03_pizza_menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import "./index.css"
 
 const pizzaData = [
 	{
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
 	return (
-		<div>
+		<div className='container'>
 			<Header />
 			<Menu />
 			<Footer />
@@ -57,19 +58,22 @@ function App() {
 }
 
 function Header() {
-	const style = { color: "red", fontSize: "32px", textTransform: "uppercase" }
-	return <h1 style={style}>Fast React Pizza Co.</h1>
+	return (
+		<header className='header'>
+			<h1>Fast React Pizza Co.</h1>
+		</header>
+	)
 }
 
 function Menu() {
 	return (
-		<div>
+		<main className='menu'>
 			<h2>Our menu</h2>
 			<Pizza />
 			<Pizza />
 			<Pizza />
 			<Pizza />
-		</div>
+		</main>
 	)
 }
 
@@ -87,7 +91,7 @@ function Footer() {
 	// 	alert("We're currently open!")
 	// }
 	return (
-		<footer>
+		<footer className='footer'>
 			{new Date().toLocaleTimeString().slice(0, -2)} We're currently open!
 		</footer>
 	)
@@ -97,7 +101,7 @@ function Pizza() {
 	return (
 		<div>
 			<img src='pizzas/spinaci.jpg' alt='Pizza spinaci' />
-			<h2>Pizza Spinaci</h2>
+			<h3>Pizza Spinaci</h3>
 			<p>Tomato, mozarella, spinach, and ricotta cheese</p>
 		</div>
 	)
