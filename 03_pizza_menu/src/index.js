48c43174609e433e60618f9dee9196d4f858e1b4
Monika <codingbymon@gmail.com>
2024-11-15 12:46:16 +0100

    Passing and Receiving Props

--- 03_pizza_menu/src/index.js
+++ 03_pizza_menu/src/index.js
@@ -69,11 +69,33 @@ function Menu() {
 	return (
 		<main className='menu'>
 			<h2>Our menu</h2>
-			<Pizza />
-			<Pizza />
-			<Pizza />
-			<Pizza />
+			<Pizza
+				name='Pizza Spinaci'
+				ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+				photoName='pizzas/spinaci.jpg'
+				price = {10}
+			/>
+			<Pizza
+				name='Pizza Fungi'
+				ingredients='Tomato, mozarella, mushrooms, and onion'
+				photoName='pizzas/funghi.jpg'
+				price='12'
+			/>
 		</main>
+	)
+}
+
+function Pizza(props) {
+	console.log(props)
+	return (
+		<div className='pizza'>
+			<img src={props.photoName} alt={props.name} />
+			<div>
+				<h3>{props.name}</h3>
+				<p>{props.ingredients}</p>
+				<span>{props.price}</span>
+			</div>
+		</div>
 	)
 }
 
@@ -94,16 +116,6 @@ function Footer() {
 		<footer className='footer'>
 			{new Date().toLocaleTimeString().slice(0, -2)} We're currently open!
 		</footer>
-	)
-}
-
-function Pizza() {
-	return (
-		<div>
-			<img src='pizzas/spinaci.jpg' alt='Pizza spinaci' />
-			<h3>Pizza Spinaci</h3>
-			<p>Tomato, mozarella, spinach, and ricotta cheese</p>
-		</div>
 	)
 }
 
