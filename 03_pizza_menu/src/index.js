b0532280006407f276d299ade22939cb7fdce895
Monika <codingbymon@gmail.com>
2024-11-22 12:20:07 +0100

    Conditional setting of classes and text

--- 03_pizza_menu/src/index.js
+++ 03_pizza_menu/src/index.js
@@ -94,17 +94,20 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-	if (pizzaObj.soldOut) {
-		return null
-	}
+	// if (pizzaObj.soldOut) {
+	// 	return null
+	// }
 
 	return (
-		<li className='pizza'>
+		<li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""} `}>
 			<img src={pizzaObj.photoName} alt={pizzaObj.name} />
 			<div>
 				<h3>{pizzaObj.name}</h3>
 				<p>{pizzaObj.ingredients}</p>
-				<span>{pizzaObj.price}</span>
+
+				{/* {pizzaObj.soldOut ? <span>Sold Out</span> : <span>pizzaObj.price</span>} */}
+
+				<span>{pizzaObj.soldOut ? "Sold out" : pizzaObj.price}</span>
 			</div>
 		</li>
 	)
@@ -149,9 +152,3 @@ root.render(
 		<App />
 	</React.StrictMode>
 )
-
-
-
-
-  
-  
