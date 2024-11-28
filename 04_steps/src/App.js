ee784dbe3d753d80261b18d95f62af7e13262e00
Monika <codingbymon@gmail.com>
2024-11-28 15:06:51 +0100

    Creating a State Variable With useState

--- 04_steps/src/App.js
+++ 04_steps/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 const messages = [
 	"Learn React ⚛️",
 	"Apply for jobs 💼",
@@ -5,22 +6,23 @@ const messages = [
 ]
 
 export default function App() {
-	const step = 1
+	const [step, setStep] = useState(1)
+
 	const style = { backgroundColor: "#7950f2", color: "#fff" }
 
-  function handleAlert(){
-    alert('Previous')
-  }
-  function handleNext(){
-    alert('Next')
-  }
+	function handlePrevious() {
+		if (step > 1) setStep(step - 1)
+	}
+	function handleNext() {
+		if (step < 3) setStep(step + 1)
+	}
 
 	return (
 		<div className='steps'>
 			<div className='numbers'>
-				<div className={`${step>=1 ? 'active' : ''}`}>1</div>
-				<div className={`${step>=2 ? 'active' : ''}`}>2</div>
-				<div className={`${step>=3 ? 'active' : ''}`}>3</div>
+				<div className={`${step >= 1 ? "active" : ""}`}>1</div>
+				<div className={`${step >= 2 ? "active" : ""}`}>2</div>
+				<div className={`${step >= 3 ? "active" : ""}`}>3</div>
 			</div>
 
 			<p className='message'>
@@ -28,8 +30,12 @@ export default function App() {
 			</p>
 
 			<div className='buttons'>
-				<button style={style} onClick={handleAlert}>Previous</button>
-				<button style={style} onClick={handleNext}>Next</button>
+				<button style={style} onClick={handlePrevious}>
+					Previous
+				</button>
+				<button style={style} onClick={handleNext}>
+					Next
+				</button>
 			</div>
 		</div>
 	)
