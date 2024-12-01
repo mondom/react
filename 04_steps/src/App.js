4f82e99fa5650844427d9d1bbc0458dd5bf6e0eb
Monika <codingbymon@gmail.com>
2024-12-01 16:59:29 +0100

    Don't Set State Manually!

--- 04_steps/src/App.js
+++ 04_steps/src/App.js
@@ -7,6 +7,7 @@ const messages = [
 
 export default function App() {
 	const [step, setState] = useState(1)
+	const [test, setTest]= useState({name: 'Monika'})
 	const style = { backgroundColor: "#7950f2", color: "#fff" }
 
 	function handlePrevious() {
@@ -14,19 +15,21 @@ export default function App() {
 	}
 	function handleNext() {
 		if (step < 3) setState(step + 1)
+			setTest({name: 'Kinga'})
 	}
 
 	return (
 		<div className='steps'>
 			<div className='numbers'>
-				<div className={`${step >= 1 ? "active" : ""}`}>1</div>
-				<div className={`${step >= 2 ? "active" : ""}`}>2</div>
-				<div className={`${step >= 3 ? "active" : ""}`}>3</div>
+				<div className={step >= 1 ? "active" : ""}>1</div>
+				<div className={step >= 2 ? "active" : ""}>2</div>
+				<div className={step >= 3 ? "active" : ""}>3</div>
 			</div>
 
 			<p className='message'>
 				Step {step} : {messages[step - 1]}
 			</p>
+			{test.name}
 
 			<div className='buttons'>
 				<button style={style} onClick={handlePrevious}>
