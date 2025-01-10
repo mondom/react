c2cc72c7dff14223bad45e424e6584e6bbe11f7f
Monika <codingbymon@gmail.com>
2025-01-10 09:56:11 +0100

    creating a draft form

--- 05_travel_list/src/App.js
+++ 05_travel_list/src/App.js
@@ -23,6 +23,13 @@ function Form () {
 	return (
 		<form className='add-form'>
 			<h3>What do you need for your trip?</h3>
+			<select>
+				<option value={1}>1</option>
+				<option value={2}>2</option>
+				<option value={3}>3</option>
+			</select>
+			<input type='text' placeholder='Item...'></input>
+			<button>ADD</button>
 		</form>
 	)
 }
