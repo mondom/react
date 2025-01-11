d3ea69ea84e2ee92b3d08dac99cf03a263780ed2
Monika <codingbymon@gmail.com>
2025-01-11 09:30:45 +0100

    Controlled Elements

--- 05_travel_list/src/App.js
+++ 05_travel_list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const initialItems = [
 	{ id: 1, description: "Passports", quantity: 2, packed: false },
 	{ id: 2, description: "Socks", quantity: 12, packed: false },
@@ -20,8 +22,11 @@ function Logo() {
 }
 
 function Form() {
+	const [description, setDescription] = useState("")
+
 	function handleSubmit(e) {
 		e.preventDefault()
+		console.log(e)
 	}
 
 	return (
