7692a922a90af60ed5dc74bf9dd43f89c174f6a7
Monika <codingbymon@gmail.com>
2025-01-12 09:40:36 +0100

    Controlled Elements

--- 05_travel_list/src/App.js
+++ 05_travel_list/src/App.js
@@ -23,6 +23,7 @@ function Logo() {
 
 function Form() {
 	const [description, setDescription] = useState("")
+	const [quantity, setQuantity] = useState(1)
 
 	function handleSubmit(e) {
 		e.preventDefault()
@@ -32,7 +33,7 @@ function Form() {
 	return (
 		<form className='add-form' onSubmit={handleSubmit}>
 			<h3>What do you need for your trip?</h3>
-			<select>
+			<select value={quantity} onChange={e => setQuantity(e.target.value)}>
 				{Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
 					<option value={num} key={num}>
 						{num}
