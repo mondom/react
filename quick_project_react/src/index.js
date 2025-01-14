f90237ae16c0a35e2b55bf15db1f6d75aa1d86b3
Monika <codingbymon@gmail.com>
2025-01-14 12:00:06 +0100

    flashcards

--- quick_project_react/src/index.js
+++ quick_project_react/src/index.js
@@ -4,93 +4,49 @@ import "./index.css"
 import { useState } from "react"
 
 function App() {
-	return <Counter />
-}
-
-function Counter() {
-	const [step, setStep] = useState(1)
-	const [count, setCount] = useState(0)
-
-	function updateDate() {
-		const baseDate = new Date()
-		baseDate.setDate(baseDate.getDate() + count)
-
-		const days = [
-			"Sunday",
-			"Monday",
-			"Tuesday",
-			"Wednesday",
-			"Thursday",
-			"Friday",
-			"Saturday",
-		]
-		const months = [
-			"January",
-			"February",
-			"March",
-			"April",
-			"May",
-			"June",
-			"July",
-			"August",
-			"September",
-			"October",
-			"November",
-			"December",
-		]
-
-		const dayOfWeek = days[baseDate.getDay()]
-		const month = months[baseDate.getMonth()]
-		const day = baseDate.getDate()
-		const year = baseDate.getFullYear()
-
-		return `${dayOfWeek} ${month} ${day} ${year}`
-	}
-
-	function handleStepMinus() {
-		if (step > 1) setStep(s => s - 1)
-	}
-	function handleStepPlus() {
-		setStep(s => s + 1)
-	}
-	function handleCountMinus() {
-		setCount(c => c - step)
-	}
-	function handleCountPlus() {
-		setCount(c => c + step)
-	}
-
 	return (
-		<div className='wrapper'>
-			<div className='step'>
-				<button className='minus' onClick={handleStepMinus}>
-					-
-				</button>
-				<p>Step: {step}</p>
-				<button className='plus' onClick={handleStepPlus}>
-					+
-				</button>
-			</div>
-			<div className='count'>
-				<button className='minus' onClick={handleCountMinus}>
-					-
-				</button>
-				<p>Count: {count}</p>
-				<button className='plus' onClick={handleCountPlus}>
-					+
-				</button>
-			</div>
-			<div>
-				<p>
-					{count === 0
-						? `Today is ${updateDate(count)}`
-						: count < 0
-						? `${Math.abs(count)} days ago was ${updateDate(count)}`
-						: `${count} days from today is ${updateDate(count)}`}
-				</p>
-			</div>
+		<div className='App'>
+			<FlashCards />
 		</div>
 	)
+}
+
+const questions = [
+	{
+		id: 3457,
+		question: "What language is React based on?",
+		answer: "JavaScript",
+	},
+	{
+		id: 7336,
+		question: "What are the building blocks of React apps?",
+		answer: "Components",
+	},
+	{
+		id: 8832,
+		question: "What's the name of the syntax we use to describe a UI in React?",
+		answer: "JSX",
+	},
+	{
+		id: 1297,
+		question: "How to pass data from parent to child components?",
+		answer: "Props",
+	},
+	{
+		id: 9103,
+		question: "How to give components memory?",
+		answer: "useState hook",
+	},
+	{
+		id: 2002,
+		question:
+			"What do we call an input element that is completely synchronised with state?",
+		answer: "Controlled element",
+	},
+]
+
+function FlashCards() {
+	return <div>TODO</div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
