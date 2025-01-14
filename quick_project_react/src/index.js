d1c9c486aa866f013f9cafbfaed4b67e72cac2b0
Monika <codingbymon@gmail.com>
2025-01-14 15:44:54 +0100

    capturing and updating the id

--- quick_project_react/src/index.js
+++ quick_project_react/src/index.js
@@ -46,14 +46,25 @@ const questions = [
 ]
 
 function FlashCards() {
-	
 	const [selectedId, setSelectedId] = useState(null)
+
+	function handleClick(id){
+		setSelectedId(id)
+	}
 
 	return (
 		<div className='flashcards'>
 			{questions.map(question => (
-				<div key={question.id}>
-					<p> question={question} </p>
+				<div
+					key={question.id}
+					className={question.id === selectedId ? "selected" : ""}
+					onClick={()=> handleClick(question.id)}
+				>
+					<p>
+						{question.id === selectedId
+							? question.answer
+							: question.question}
+					</p>
 				</div>
 			))}
 		</div>
